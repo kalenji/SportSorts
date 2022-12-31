8473f2502311d4611ceda81dbf75955a978510f2
Kalin <kalingavrilov@abv.bg>
2022-12-31 11:39:33 +0200

    nothing

--- SportSorts/View Controllers/HomeViewController.swift
+++ SportSorts/View Controllers/HomeViewController.swift
@@ -4,7 +4,6 @@
 //
 //  Created by Kalin Gavrilov on 27.12.22.
 //
-
 import UIKit
 
 class HomeViewController: UIViewController {
@@ -18,7 +17,6 @@ class HomeViewController: UIViewController {
 
     /*
     // MARK: - Navigation
-
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destination.
