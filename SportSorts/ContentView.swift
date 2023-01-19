b84af593d4e69b834e0cfddc30b2da32a118971f
Kalin <kalingavrilov@abv.bg>
2023-01-19 22:00:20 +0200

    creating login and register method for the 100th time

--- SportSorts/ContentView.swift
+++ SportSorts/ContentView.swift
@@ -8,14 +8,47 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var email = ""
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            VStack {
+                Image("logo ")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 150, height: 150 )
+                
+                VStack {
+                    TextField("Email Address", text: $email)
+                        .padding()
+                        .background(Color(.secondarySystemBackground))
+                        .cornerRadius(20)
+                    
+                    SecureField("Email Address", text: $email)
+                        .padding()
+                        .background(Color(.secondarySystemBackground))
+                        .cornerRadius(20)
+                    
+                    Button {
+                        
+                    } label: {
+                        Text("Sign In")
+                            .foregroundColor(Color.white)
+                            .frame(width: 200, height: 50)
+                            .background(Color.orange)
+                            .cornerRadius(20)
+                    }
+
+                    
+                }
+                .padding()
+                
+                Spacer()
+                
+            }
+            .navigationTitle("Sign In")
         }
-        .padding()
     }
 }
 
