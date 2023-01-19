618ae08f87d9945f1052fd0f160e77fbf5a976e2
Kalin <kalingavrilov@abv.bg>
2023-01-20 00:23:04 +0200

    AREEEEEEEEEEE done

--- SportSorts/ContentView.swift
+++ SportSorts/ContentView.swift
@@ -6,49 +6,192 @@
 //
 
 import SwiftUI
+import FirebaseAuth
+
+class AppViewModel: ObservableObject {
+    
+    let auth = Auth.auth()
+    
+    @Published var signedIn = false
+    
+    var isSignedIn: Bool {
+        return auth.currentUser != nil
+    }
+    
+    func signIn(email: String, password: String){
+        auth.signIn(withEmail: email, password: password){ [weak self] result, error in
+            guard result != nil, error == nil else{
+                return
+            }
+            
+            DispatchQueue.main.async {
+                self?.signedIn = true
+            }
+        }
+    }
+    
+    func signUp(email: String, password: String){
+        auth.createUser(withEmail: email, password: password){ [weak self] result, error in
+            guard result != nil, error == nil else{
+                return
+            }
+            
+            DispatchQueue.main.async {
+                self?.signedIn = true
+            }
+        }
+    }
+    
+    func signOut() {
+        try? auth.signOut()
+        
+        self.signedIn = false
+    }
+}
 
 struct ContentView: View {
     
-    @State var email = ""
+    @EnvironmentObject var viewModel: AppViewModel
     
     var body: some View {
         NavigationView {
-            VStack {
-                Image("logo ")
-                    .resizable()
-                    .scaledToFit()
-                    .frame(width: 150, height: 150 )
-                
+            if viewModel.signedIn {
                 VStack {
-                    TextField("Email Address", text: $email)
-                        .padding()
-                        .background(Color(.secondarySystemBackground))
-                        .cornerRadius(20)
-                    
-                    SecureField("Email Address", text: $email)
-                        .padding()
-                        .background(Color(.secondarySystemBackground))
-                        .cornerRadius(20)
+                    Text("Signed In")
                     
                     Button {
-                        
+                        viewModel.signOut()
                     } label: {
-                        Text("Sign In")
-                            .foregroundColor(Color.white)
+                        Text("Sign Out")
                             .frame(width: 200, height: 50)
                             .background(Color.orange)
+                            .foregroundColor(Color.white)
                             .cornerRadius(20)
+                            .padding()
                     }
+                }
+
+            }else{
+                SignInView()
+            }
+        }
+        .onAppear {
+            viewModel.signedIn = viewModel.isSignedIn
+        }
+    }
+}
+
+struct SignInView: View {
+    
+    @State var email = ""
+    @State var password = ""
+    
+    @EnvironmentObject var viewModel: AppViewModel
+    
+    var body: some View {
+        VStack {
+            Image("logo")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 150, height: 150 )
+                
+            VStack {
+                TextField("Email Address", text: $email)
+                    .disableAutocorrection(true)
+                    .autocapitalization(.none)
+                    .padding()
+                    .background(Color(.secondarySystemBackground))
+                    .cornerRadius(20)
+                    
+                SecureField("Password", text: $password)
+                    .disableAutocorrection(true)
+                    .autocapitalization(.none)
+                    .padding()
+                    .background(Color(.secondarySystemBackground))
+                    .cornerRadius(20)
+                    
+                Button {
+                        
+                    guard !email.isEmpty, !password.isEmpty else {
+                        return
+                    }
+                    
+                    viewModel.signIn(email: email, password: password)
+                        
+                } label: {
+                    Text("Sign In")
+                        .foregroundColor(Color.white)
+                        .frame(width: 200, height: 50)
+                        .background(Color.orange)
+                        .cornerRadius(20)
+                }
+
+                NavigationLink("Create Account", destination: SignUpView())
+                    .padding()
+                    
+            }
+            .padding()
+            
+            Spacer()
+                
+        }
+        .navigationTitle("Sign In")
+    }
+}
+
+struct SignUpView: View {
+    
+    @State var email = ""
+    @State var password = ""
+    
+    @EnvironmentObject var viewModel: AppViewModel
+    
+    var body: some View {
+        VStack {
+            Image("logo")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 150, height: 150 )
+                
+            VStack {
+                TextField("Email Address", text: $email)
+                    .disableAutocorrection(true)
+                    .autocapitalization(.none)
+                    .padding()
+                    .background(Color(.secondarySystemBackground))
+                    .cornerRadius(20)
+                    
+                SecureField("Password", text: $password)
+                    .disableAutocorrection(true)
+                    .autocapitalization(.none)
+                    .padding()
+                    .background(Color(.secondarySystemBackground))
+                    .cornerRadius(20)
+                    
+                Button {
+                        
+                    guard !email.isEmpty, !password.isEmpty else {
+                        return
+                    }
+                    
+                    viewModel.signUp(email: email, password: password)
+                        
+                } label: {
+                    Text("Create Account")
+                        .foregroundColor(Color.white)
+                        .frame(width: 200, height: 50)
+                        .background(Color.orange)
+                        .cornerRadius(20)
+                }
 
                     
-                }
-                .padding()
-                
-                Spacer()
-                
             }
-            .navigationTitle("Sign In")
+            .padding()
+            
+            Spacer()
+                
         }
+        .navigationTitle("Create Account")
     }
 }
 
