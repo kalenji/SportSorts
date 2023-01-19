b45ba53ce56294f002f08f1bda91aae5b46ff887
Kalin <kalingavrilov@abv.bg>
2023-01-19 21:40:00 +0200

    Initial Commit

---
+++ SportSorts/SportSortsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SportSortsApp.swift
+//  SportSorts
+//
+//  Created by Kalin Gavrilov on 19.01.23.
+//
+
+import SwiftUI
+
+@main
+struct SportSortsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
